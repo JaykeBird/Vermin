15913368d5e1e429b2bc7f99515d74ebb7196c14
TinyTom99 <trismeyer@gmail.com>
2015-04-02 18:17:05 -0500

    Initial Setup
    Created small floor and Capsule Character
    Added Controls for the Character

---
+++ Assets/Physics Platformer Kit/Scripts/DetectPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPlayer : MonoBehaviour {
+
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    //appear when player is present
+    void OnTriggerEnter(Collider other)
+    {
+        enemy1.SetActive(true);
+        enemy2.SetActive(true);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
