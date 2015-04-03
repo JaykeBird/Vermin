16812ab4192f7eb7338bb4e167b4c9a47fbb5ae9
TinyTom99 <trismeyer@gmail.com>
2015-04-03 18:01:42 -0500

    Added Item Script
    Created Item script, Added functionality similar to Coin
    Edited GUIManager to include Item List

--- Assets/Physics Platformer Kit/Scripts/GUIManager.cs
+++ Assets/Physics Platformer Kit/Scripts/GUIManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 //ATTACH TO MAIN CAMERA, shows your health and coins
 public class GUIManager : MonoBehaviour 
@@ -8,16 +9,19 @@ public class GUIManager : MonoBehaviour
 	[HideInInspector]
     public int coinsCollected=0;
     public int coinsInStash1=0;
+    public List<bool> inventoryCheck = new List<bool>(8);       //this will show which items the player currently has
 
 	private int coinsInLevel;
+    private int itemsInLevel;
 	
 	//setup, get how many coins are in this level
 	void Start()
     {
         coinsInLevel = GameObject.FindGameObjectsWithTag("Coin").Length;
+        itemsInLevel = GameObject.FindGameObjectsWithTag("Item").Length;
 	}
 	
-	//show current health and how many coins you've collected
+	//show and how many coins you've collected
 	void OnGUI()
 	{
 		GUI.skin = guiSkin;
@@ -27,6 +31,10 @@ public class GUIManager : MonoBehaviour
         {
             GUILayout.Label("Coins: " + coinsCollected + " / " + coinsInLevel);
             GUILayout.Label("Stash One: " + coinsInStash1);
+        }
+
+        if (itemsInLevel > 0)
+        {
         }
 	}
 }
---
+++ Assets/Physics Platformer Kit/Scripts/Item.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//class to add to collectible coins
+[RequireComponent(typeof(SphereCollider))]
+public class Item : MonoBehaviour
+{
+    public AudioClip collectSound;							//sound to play when item is collected
+    public Vector3 rotation = new Vector3(0, 80, 0);		//idle rotation of item
+    public Vector3 rotationGain = new Vector3(0, 20, 00);	//added rotation when player gets near item 
+    public float startSpeed = 3f;							//how fast item moves toward player when they get near
+    public float speedGain = 0.2f;							//how fast item accelerates toward player when they're near
+    public Texture2D sprite;
+
+    private bool collected;
+    private Transform player;
+    private TriggerParent triggerParent;	//this is a utility class, that lets us check if the player is close to the items "bounds sphere trigger"
+    private GUIManager gui;
+    private static List<Item> inventory = new List<Item>(8);
+
+
+    //setup
+    void Awake()
+    {
+        gui = FindObjectOfType(typeof(GUIManager)) as GUIManager;
+        if (tag != "Item")
+        {
+            tag = "Item";
+            Debug.LogWarning("'Item' script attached to object not tagged 'Item', tag added automatically", transform);
+        }
+        collider.isTrigger = true;
+        triggerParent = GetComponentInChildren<TriggerParent>();
+        //if no trigger bounds are attached to coin, set them up
+        if (!triggerParent)
+        {
+            GameObject bounds = new GameObject();
+            bounds.name = "Bounds";
+            bounds.AddComponent("SphereCollider");
+            bounds.GetComponent<SphereCollider>().radius = 2f;
+            bounds.GetComponent<SphereCollider>().isTrigger = true;
+            bounds.transform.parent = transform;
+            bounds.transform.position = transform.position;
+            bounds.AddComponent("TriggerParent");
+            triggerParent = GetComponentInChildren<TriggerParent>();
+            triggerParent.tagsToCheck = new string[1];
+            triggerParent.tagsToCheck[0] = "Player";
+            Debug.LogWarning("No pickup radius 'bounds' trigger attached to item: " + transform.name + ", one has been added automatically", bounds);
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    //move item toward player when he is close to it, and increase the spin/speed of the item
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime, Space.World);
+
+        if (triggerParent.collided)
+            collected = true;
+
+        if (collected)
+        {
+            startSpeed += speedGain;
+            rotation += rotationGain;
+            transform.position = Vector3.Lerp(transform.position, player.position, startSpeed * Time.deltaTime);
+        }
+    }
+
+    //give player coin when it touches them
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            ItemGet();
+    }
+
+    void ItemGet()
+    {
+        if (collectSound)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+        if (gui && sprite)
+        {
+            if (this.name.Equals("Glue Bottle"))   //Glue bottle is item number 0
+            {
+                Item.inventory.Insert(0, this);
+                gui.inventoryCheck.Insert(0, true);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
