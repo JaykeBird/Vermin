b21e371219bdcce5ba6697de58fcec53197e444e
Jayke R. Huempfner <jacobrh94@hotmail.com>
2015-04-17 17:07:16 -0500

    Added main menu
    Made a new Main Menu scene and updated some parts of the Project Settings.

---
+++ Assets/Physics Platformer Kit/Scripts/PlayScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void ClickPlay()
+    {
+        Debug.Log("Let's play!");
+
+        // Physics Platformer Set/Scenes/OfficeSpaceFloor1.unity
+        Application.LoadLevel("OfficeSpaceFloor1");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("Let's exit!");
+
+        Application.Quit();
+    }
+}
