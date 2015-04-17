d4ea4e57f443ee859c55b78a5294d4d8fa4b2e65
PepsiDragon <rainick987@yahoo.com>
2015-04-17 18:42:41 -0500

    TrapTimer script
    I have spoke it into existence, whatever the fuck that means.

---
+++ Assets/Physics Platformer Kit/Scripts/TrapTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTimer : MonoBehaviour {
+
+	public float TrapTime; //the time this trap has to live on this world.
+	private float TimeHappened; //the time this trap has lived on this world. 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimeHappened = TimeHappened + Time.deltaTime;
+		if (TimeHappened >= TrapTime) 
+		{
+			Destroy (this);
+		}
+	}
+}
