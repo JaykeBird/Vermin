e4bc1bb08cfd0a5ec10b4716523a11da5221de1c
Jayke R. Huempfner <jacobrh94@hotmail.com>
2015-05-01 22:55:36 -0500

    Updated main menu!
    You can now select which class to play as. Also, fancy animations are fancy.
    
    I learned how to animate! For real this time. ;)

--- Assets/Physics Platformer Kit/Scripts/PlayScript.cs
+++ Assets/Physics Platformer Kit/Scripts/PlayScript.cs
@@ -4,6 +4,9 @@ using Assets.Physics_Platformer_Kit.Scripts;
 
 public class PlayScript : MonoBehaviour {
 
+    public Animator charSelect;
+    public Animator panel;
+
     // Use this for initialization
     void Start () {
     
@@ -18,16 +21,49 @@ public class PlayScript : MonoBehaviour {
     {
         Debug.Log("Let's play!");
 
+        charSelect.SetBool("IsShown", true);
+        panel.SetBool("IsShown", false);
+
         // Physics Platformer Set/Scenes/OfficeSpaceFloor1.unity
-        Application.LoadLevel("OfficeSpaceFloor1");
+        //Application.LoadLevel("OfficeSpaceFloor1");
     }
 
     public void ClickExit()
     {
         Debug.Log("Let's exit!");
 
+        panel.SetBool("IsShown", false);
+
         Application.Quit();
         
+    }
+
+    public void StartGame(string cls)
+    {
+        switch (cls.ToLower())
+        {
+            case "squirrel":
+                ChangeClass(CharacterClassData.characterClass.SQUIRREL);
+                break;
+            case "raccoon":
+                ChangeClass(CharacterClassData.characterClass.RACCOON);
+                break;
+            case "ferret":
+                ChangeClass(CharacterClassData.characterClass.FERRET);
+                break;
+            case "birdy":
+                ChangeClass(CharacterClassData.characterClass.BIRD);
+                break;
+        }
+
+        charSelect.SetBool("IsShown", false);
+        Application.LoadLevel("OfficeSpaceFloor1");
+    }
+
+    public void GoBackHome()
+    {
+        charSelect.SetBool("IsShown", false);
+        panel.SetBool("IsShown", true);
     }
 
     public void ChangeClass(CharacterClassData.characterClass value)
