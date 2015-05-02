644f72eb8a938dbf03e6475cf6621dff0e2a6a2c
Jayke R. Huempfner <jacobrh94@hotmail.com>
2015-05-01 21:26:48 -0500

    yay?

--- Assets/Physics Platformer Kit/Scripts/CharacterClassData.cs
+++ Assets/Physics Platformer Kit/Scripts/CharacterClassData.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -101,4 +101,4 @@ namespace Assets.Physics_Platformer_Kit.Scripts
             }
         }
     }
-}*/
+}
